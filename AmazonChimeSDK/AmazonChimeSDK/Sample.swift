8c275889696c25efe8f43ad4c5b72118f81be1be
Haoran Wang <wanghaor@amazon.com>
2020-01-07 14:38:39 -0800

    Setup SDK framework project and demo app

---
+++ AmazonChimeSDK/AmazonChimeSDK/Sample.swift
@@ -0,0 +1,16 @@
+//
+//  Sample.swift
+//  AmazonChimeSDK
+//
+//  Created by Wang, Haoran on 1/6/20.
+//  Copyright © 2020 Amazon Chime. All rights reserved.
+//
+
+import AmazonChimeSDK.Video
+import Foundation
+
+public class Sample {
+    public init() {
+        let sample = video_capture_device_s()
+    }
+}
---
+++ AmazonChimeSDK/AmazonChimeSDKTests/AmazonChimeSDKTests.swift
@@ -0,0 +1,34 @@
+//
+//  AmazonChimeSDKTests.swift
+//  AmazonChimeSDKTests
+//
+//  Created by Wang, Haoran on 1/6/20.
+//  Copyright © 2020 Amazon Chime. All rights reserved.
+//
+
+import XCTest
+@testable import AmazonChimeSDK
+
+class AmazonChimeSDKTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
