759b309d22170db4e45d0802700ba604ee051c20
Haoran Wang <wanghaor@amazon.com>
2020-02-13 15:31:14 -0800

    Use signal strength in demo app

---
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/SignalStatusCode.swift
@@ -0,0 +1,14 @@
+//
+//  SignalStatusCode.swift
+//  AmazonChimeSDKDemo
+//
+//  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+
+import Foundation
+
+enum SignalStatusCode: Int {
+    case badSignal = 0
+    case unstableSignal = 1
+    case goodSignal = 2
+}
--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/data/RosterAttendee.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/data/RosterAttendee.swift
@@ -10,9 +10,11 @@ import Foundation
 public class RosterAttendee {
     let name: String
     var volume: Int
+    var signal: Int
 
-    init(name: String, volume: Int) {
+    init(name: String, volume: Int, signal: Int) {
         self.name = name
         self.volume = volume
+        self.signal = signal
     }
 }
