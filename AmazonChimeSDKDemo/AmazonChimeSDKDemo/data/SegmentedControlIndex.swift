a1e2c468c6f44d2059f30a261ec614f01908f9d0
Haoran Wang <wanghaor@amazon.com>
2020-02-24 12:11:17 -0800

    Add 1:1 video call to demo app

---
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/data/SegmentedControlIndex.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentedControlIndex.swift
+//  AmazonChimeSDKDemo
+//
+//  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+
+import Foundation
+
+enum SegmentedControlIndex: Int {
+    case roster = 0
+    case videos = 1
+}
