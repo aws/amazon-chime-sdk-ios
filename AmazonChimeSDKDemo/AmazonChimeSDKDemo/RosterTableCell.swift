3bbfe5faeea8c8614d0173639d6151a3020d9a80
Zhinan Guan <62256490+zhinang-amazon@users.noreply.github.com>
2020-06-25 17:23:28 -0700

    Extract RosterModel out of MeetingViewController (#185)

--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/RosterTableCell.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/RosterTableCell.swift
@@ -6,7 +6,10 @@
 //  SPDX-License-Identifier: Apache-2.0
 //
 
+import AmazonChimeSDK
 import UIKit
+
+let rosterTableCellReuseIdentifier: String = "rosterCell"
 
 class RosterTableCell: UITableViewCell {
     @IBOutlet var attendeeName: UILabel!
@@ -14,4 +17,60 @@ class RosterTableCell: UITableViewCell {
     @IBOutlet var metricName: UILabel!
     @IBOutlet var metricValue: UILabel!
     @IBOutlet var speakLevel: UIImageView!
+
+    init(attendee: RosterAttendee, isActiveSpeaker: Bool) {
+        super.init(style: .default, reuseIdentifier: rosterTableCellReuseIdentifier)
+
+        updateCell(attendee: attendee, isActiveSpeaker: isActiveSpeaker)
+    }
+
+    func updateCell(attendee: RosterAttendee, isActiveSpeaker: Bool) {
+        attendeeName.text = attendee.attendeeName
+        attendeeName.accessibilityIdentifier = attendee.attendeeName
+
+        if attendee.volume == .notSpeaking {
+            accessibilityIdentifier = "\(attendee.attendeeName ?? "") Not Speaking"
+        } else if attendee.volume == .muted {
+            accessibilityIdentifier = "\(attendee.attendeeName ?? "") Muted"
+        } else {
+            accessibilityIdentifier = "\(attendee.attendeeName ?? "") Speaking"
+        }
+
+        indicator.isHidden = !isActiveSpeaker
+        indicator.layer.cornerRadius = indicator.frame.size.width / 2.0
+        speakLevel.tintColor = .systemGray
+
+        speakLevel.image = getSpeakLevelImage(signal: attendee.signal, volume: attendee.volume)
+    }
+
+    private func getSpeakLevelImage(signal: SignalStrength, volume: VolumeLevel) -> UIImage? {
+        var image: UIImage?
+        if signal != .high {
+            if volume == .muted {
+                image = UIImage(named: "signal-poor-muted")
+            } else {
+                image = UIImage(named: "signal-poor")
+            }
+        } else {
+            switch volume {
+            case .muted:
+                image = UIImage(named: "volume-muted")
+            case .notSpeaking:
+                image = UIImage(named: "volume-0")
+            case .low:
+                image = UIImage(named: "volume-1")
+            case .medium:
+                image = UIImage(named: "volume-2")
+            case .high:
+                image = UIImage(named: "volume-3")
+            @unknown default:
+                break
+            }
+        }
+        return image?.withRenderingMode(.alwaysTemplate)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
