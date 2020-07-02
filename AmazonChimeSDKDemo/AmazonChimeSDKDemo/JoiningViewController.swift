6235c12da72c4c03013b44da9117b25bd9ac1c4e
Zhinan Guan <62256490+zhinang-amazon@users.noreply.github.com>
2020-07-02 09:53:19 -0700

    Add join meeting buttons through CallKit (#194)
    * Add join meeting buttons through CallKit
    
    * Update text for join buttons
    
    * Move CallKitOption to separate file

--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/JoiningViewController.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/JoiningViewController.swift
@@ -12,29 +12,28 @@ import Toast
 import UIKit
 
 class JoiningViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet var meetingIDText: UITextField!
-    @IBOutlet var nameText: UITextField!
+    @IBOutlet var meetingIdTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
     @IBOutlet var joinButton: UIButton!
     @IBOutlet var versionLabel: UILabel!
+    @IBOutlet var joinWithoutCallKitButton: UIButton!
+    @IBOutlet var joinAsIncomingCallButton: UIButton!
+    @IBOutlet var joinAsOutgoingCallButton: UIButton!
 
-    var meetingID = ""
-    var name = ""
-    let logger = ConsoleLogger(name: "JoiningViewController")
+    private let logger = ConsoleLogger(name: "JoiningViewController")
+    private let toastDisplayDuration = 2.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        meetingIDText.delegate = self
-        nameText.delegate = self
+        meetingIdTextField.delegate = self
+        nameTextField.delegate = self
 
         setupHideKeyboardOnTap()
         versionLabel.text = "amazon-chime-sdk-ios@\(Versioning.sdkVersion())"
-        joinButton.setImage(UIImage(named: "join")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        joinButton.tintColor = .systemGray
-        joinButton.accessibilityLabel = "Join meeting"
     }
 
     override func viewWillAppear(_: Bool) {
-        joinButton.isEnabled = true
+        setJoinButtons(isEnabled: true)
     }
 
     private func urlRewriter(url: String) -> String {
@@ -43,57 +42,84 @@ class JoiningViewController: UIViewController, UITextFieldDelegate {
         return url
     }
 
-    @IBAction func joinMeeting(sender _: UIButton) {
-        view.endEditing(true)
-        joinButton.isEnabled = false
-        meetingID = meetingIDText.text ?? ""
-        name = nameText.text ?? ""
-
-        postRequest(completion: { data, error in
-            guard error == nil else {
-                DispatchQueue.main.async {
-                    self.view.makeToast("Unable to join meeting please try different meeting ID", duration: 2.0)
-                    self.joinButton.isEnabled = true
-                }
-                return
-            }
-
-            if let data = data {
-                let (meetingResp, attendeeResp) = self.processJson(data: data)
-                guard let currentMeetingResponse = meetingResp, let currentAttendeeResponse = attendeeResp else {
-                    return
-                }
-
-                let meetingSessionConfig = MeetingSessionConfiguration(createMeetingResponse: currentMeetingResponse,
-                                                                       createAttendeeResponse: currentAttendeeResponse,
-                                                                       urlRewriter: self.urlRewriter)
-                DispatchQueue.main.async {
-                    let meetingView = UIStoryboard(name: "Main", bundle: nil)
-                    guard let meetingViewController = meetingView.instantiateViewController(withIdentifier: "meeting")
-                        as? MeetingViewController else {
-                        self.logger.error(msg: "Unable to instantitateViewController")
-                        return
-                    }
-                    meetingViewController.modalPresentationStyle = .fullScreen
-                    meetingViewController.meetingSessionConfig = meetingSessionConfig
-                    meetingViewController.meetingId = self.meetingID
-                    meetingViewController.selfName = self.name
-                    self.present(meetingViewController, animated: true, completion: nil)
-                }
-            }
-        })
+    @IBAction func joinWithoutCallKitButtonClicked(_ sender: UIButton) {
+        joinMeeting(callKitOption: .disabled)
     }
 
-    private func postRequest(completion: @escaping CompletionFunc) {
-        if meetingID.isEmpty || name.isEmpty {
+    @IBAction func joinAsIncomingCallButton(_ sender: UIButton) {
+        joinMeeting(callKitOption: .incoming)
+    }
+
+    @IBAction func joinAsOutgoingCallButton(_ sender: UIButton) {
+        joinMeeting(callKitOption: .outgoing)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    private func setJoinButtons(isEnabled: Bool) {
+        joinWithoutCallKitButton.isEnabled = isEnabled
+        joinAsIncomingCallButton.isEnabled = isEnabled
+        joinAsOutgoingCallButton.isEnabled = isEnabled
+    }
+
+    func joinMeeting(callKitOption: CallKitOption) {
+        view.endEditing(true)
+        let meetingId = meetingIdTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+
+        if meetingId.isEmpty || name.isEmpty {
             DispatchQueue.main.async {
-                self.view.makeToast("MeetingID or name is invalid", duration: 2.0)
+                self.view.makeToast("Meeting ID or name is invalid",
+                                    duration: self.toastDisplayDuration)
             }
             return
         }
 
+        setJoinButtons(isEnabled: false)
+
+        postRequest(meetingId: meetingId, name: name, completion: { data, error in
+            guard error == nil, let data = data else {
+                DispatchQueue.main.async {
+                    self.view.makeToast("Unable to join meeting please try different meeting ID",
+                                        duration: self.toastDisplayDuration)
+                    self.setJoinButtons(isEnabled: true)
+                }
+                return
+            }
+
+            let (meetingResp, attendeeResp) = self.processJson(data: data)
+            guard let currentMeetingResponse = meetingResp, let currentAttendeeResponse = attendeeResp else {
+                self.logger.error(msg: "Unable to process meeting JSON response")
+                return
+            }
+
+            let meetingSessionConfig = MeetingSessionConfiguration(createMeetingResponse: currentMeetingResponse,
+                                                                   createAttendeeResponse: currentAttendeeResponse,
+                                                                   urlRewriter: self.urlRewriter)
+
+            DispatchQueue.main.async {
+                let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                guard let meetingViewController = mainStoryboard.instantiateViewController(withIdentifier: "meeting")
+                    as? MeetingViewController else {
+                    self.logger.error(msg: "Unable to instantitate MeetingViewController")
+                    return
+                }
+                meetingViewController.modalPresentationStyle = .fullScreen
+                meetingViewController.meetingSessionConfig = meetingSessionConfig
+                meetingViewController.meetingId = meetingId
+                meetingViewController.selfName = name
+                meetingViewController.callKitOption = callKitOption
+                self.present(meetingViewController, animated: true, completion: nil)
+            }
+        })
+    }
+
+    private func postRequest(meetingId: String, name: String, completion: @escaping CompletionFunc) {
         let encodedURL = HttpUtils.encodeStrForURL(
-            str: "\(AppConfiguration.url)join?title=\(meetingID)&name=\(name)&region=\(AppConfiguration.region)"
+            str: "\(AppConfiguration.url)join?title=\(meetingId)&name=\(name)&region=\(AppConfiguration.region)"
         )
         HttpUtils.postRequest(
             url: encodedURL,
@@ -106,24 +132,26 @@ class JoiningViewController: UIViewController, UITextFieldDelegate {
         let jsonDecoder = JSONDecoder()
         do {
             let joinMeetingResponse = try jsonDecoder.decode(JoinMeetingResponse.self, from: data)
+            let meeting = joinMeetingResponse.joinInfo.meeting.meeting
             let meetingResp = CreateMeetingResponse(meeting:
                 Meeting(
-                    externalMeetingId: joinMeetingResponse.joinInfo.meeting.meeting.externalMeetingId,
+                    externalMeetingId: meeting.externalMeetingId,
                     mediaPlacement: MediaPlacement(
-                        audioFallbackUrl: joinMeetingResponse.joinInfo.meeting.meeting.mediaPlacement.audioFallbackUrl,
-                        audioHostUrl: joinMeetingResponse.joinInfo.meeting.meeting.mediaPlacement.audioHostUrl,
-                        signalingUrl: joinMeetingResponse.joinInfo.meeting.meeting.mediaPlacement.signalingUrl,
-                        turnControlUrl: joinMeetingResponse.joinInfo.meeting.meeting.mediaPlacement.turnControlUrl
+                        audioFallbackUrl: meeting.mediaPlacement.audioFallbackUrl,
+                        audioHostUrl: meeting.mediaPlacement.audioHostUrl,
+                        signalingUrl: meeting.mediaPlacement.signalingUrl,
+                        turnControlUrl: meeting.mediaPlacement.turnControlUrl
                     ),
-                    mediaRegion: joinMeetingResponse.joinInfo.meeting.meeting.mediaRegion,
-                    meetingId: joinMeetingResponse.joinInfo.meeting.meeting.meetingId
+                    mediaRegion: meeting.mediaRegion,
+                    meetingId: meeting.meetingId
                 )
             )
+            let attendee = joinMeetingResponse.joinInfo.attendee.attendee
             let attendeeResp = CreateAttendeeResponse(attendee:
-                Attendee(attendeeId: joinMeetingResponse.joinInfo.attendee.attendee.attendeeId,
-                         externalUserId: joinMeetingResponse.joinInfo.attendee.attendee.externalUserId,
-                         joinToken: joinMeetingResponse.joinInfo.attendee.attendee.joinToken))
-
+                Attendee(attendeeId: attendee.attendeeId,
+                         externalUserId: attendee.externalUserId,
+                         joinToken: attendee.joinToken)
+            )
             return (meetingResp, attendeeResp)
         } catch {
             logger.error(msg: error.localizedDescription)
---
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/utils/CallKitOption.swift
@@ -0,0 +1,13 @@
+//
+//  CallKitOption.swift
+//  AmazonChimeSDKDemo
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+enum CallKitOption {
+    case disabled
+    case incoming
+    case outgoing
+}
