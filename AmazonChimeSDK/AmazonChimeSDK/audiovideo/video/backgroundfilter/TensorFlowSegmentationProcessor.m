071730ba15ce655f90dffa728f00d2dfb03e9207
Richard Huang <richhx@amazon.com>
2022-05-01 16:48:54 -0700

    Add background blur and replacement processors

---
+++ AmazonChimeSDK/AmazonChimeSDK/audiovideo/video/backgroundfilter/TensorFlowSegmentationProcessor.m
@@ -0,0 +1,125 @@
+//
+//  TensorFlowSegmentationProcessor.m
+//  AmazonChimeSDK
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+#import <AmazonChimeSDK/AmazonChimeSDK-Swift.h>
+#import "TensorFlowSegmentationProcessor.h"
+#import "CwtEnum.h"
+#import "CwtTfLiteModel.h"
+
+// Implement SegmentationProcessor.
+@interface TensorFlowSegmentationProcessor () <SegmentationProcessor>
+@end
+
+// TensorFlowSegmentationProcessor is a wrapper class around AmazonChimeSDKMachineLearning
+// frameworks's implementation of CwtTfLiteModel, which is a TensorFlow Lite
+// implementation of a segmentation processor. Note that users of this class must
+// check `isAvailable` before using this processor. See `isAvailable` for more
+// details.
+@implementation TensorFlowSegmentationProcessor {
+    CwtTfLiteModel* _model;
+    NSInteger _modelState;
+}
+
+// getModelFromBundle returns the path to the segmentation model.
++ (NSString*) getModelFromBundle {
+    NSString* bundle = [[NSBundle bundleForClass:TensorFlowSegmentationProcessor.class]
+                        pathForResource:@"selfie_segmentation_landscape" ofType:@"tflite"];
+    return bundle;
+}
+
+// isAvailable returns YES if AmazonChimeSDKMachineLearning framework (i.e.
+// CwtTfLiteModel class symbol) is available in the runtime. Returns NO
+// otherwise. If AmazonChimeSDKMachineLearning is not available, all methods
+// in this class are unusable. Callers must guard the usage of this class
+// with a check to `isAvailable` first.
++ (BOOL) isAvailable {
+    Class clazz = NSClassFromString(@"CwtTfLiteModel");
+    if (clazz == nil) {
+        return NO;
+    }
+    NSString* bundle = [TensorFlowSegmentationProcessor getModelFromBundle];
+    if (!bundle) {
+        NSLog(@"Unable to find selfie segmentation model");
+        return NO;
+    }
+    return YES;
+}
+
+// init instantiates the segmentation processor.
+- (id) init {
+    _model = [[NSClassFromString(@"CwtTfLiteModel") alloc] init];;
+    _modelState = EMPTY;
+    return self;
+}
+
+// initialize instantiates the model for the segmentation processor.
+// Returns whether able to successfully initialize.
+- (BOOL) initializeWithHeight:(NSInteger)height width:(NSInteger)width channels:(NSInteger)channels {
+    NSString* bundle = [TensorFlowSegmentationProcessor getModelFromBundle];
+    if (!bundle) {
+        NSLog(@"Unable to find selfie segmentation model");
+        return NO;
+    }
+
+    // Initialize parameters to load model.
+    NSString* path = [[NSURL fileURLWithPath:bundle] path];
+    CwtInputModelConfig modelConfig;
+    modelConfig.in_height = (int)height;
+    modelConfig.in_width = (int)width;
+    modelConfig.in_channels = (int)channels;
+    modelConfig.model_range_min = 0;
+    modelConfig.model_range_max = 1;
+
+    // Get the method signature and create an invocation of it.
+    SEL selector = NSSelectorFromString(@"loadFile:config:");
+    NSMethodSignature* methodSignature = [_model methodSignatureForSelector:selector];
+    NSInvocation* invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+    [invocation setTarget:_model];
+    [invocation setSelector:selector];
+
+    // Invoke the method.
+    [invocation setArgument:&path atIndex:2];
+    [invocation setArgument:&modelConfig atIndex:3];
+    [invocation retainArguments];
+    [invocation invoke];
+
+    // Get the result.
+    NSInteger result;
+    [invocation getReturnValue:&result];
+    _modelState = result;
+    return _modelState == LOADED;
+}
+
+// Calls the model to predict the foreground on the image stored in
+// `[model getInputBuffer]`. Returns YES if succeeded. NO otherwise.
+- (BOOL) predict {
+    CwtPredictResult result = [_model predict];
+    return result == SUCCESS;
+}
+
+// Returns the integer representation of the model's currently
+// loaded state.
+// 0 = EMPTY
+// 1 = LOADING
+// 2 = LOADED
+- (NSInteger) getModelState {
+    return _modelState;
+}
+
+// Returns a direct pointer to the input buffer used to store the image data
+// that we be predicted.
+- (uint8_t* _Nonnull) getInputBuffer {
+    return [_model getInputBuffer];
+}
+
+// Returns a direct pointer to the output buffer of the predicted image.
+- (uint8_t* _Nonnull) getOutputBuffer {
+    return [_model getOutputBuffer];
+}
+
+@end  // TensorFlowSegmentationProcessor
---
+++ AmazonChimeSDK/AmazonChimeSDK/audiovideo/video/backgroundfilter/backgroundreplacement/BackgroundReplacementVideoFrameProcessor.swift
@@ -0,0 +1,150 @@
+//
+//  BackgroundReplacementVideoFrameProcessor.swift
+//  AmazonChimeSDK
+//
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+import CoreImage
+import Foundation
+import UIKit
+
+/// `BackgroundReplacementVideoFrameProcessor` is a processor which receives video frames via `VideoSource`
+///  and then creates the foreground image which is rendered on top of a background image.
+@objcMembers public class BackgroundReplacementVideoFrameProcessor: VideoSource, VideoSink {
+    public var videoContentHint = VideoContentHint.motion
+
+    /// Context used for processing and rendering the final output image.
+    private let context = CIContext(options: [.cacheIntermediates: false])
+
+    /// Set of `VideoSource` sinks.
+    private let sinks = ConcurrentMutableSet()
+
+    /// Background replacement image.
+    private var backgroundReplacementImage: CIImage?
+
+    /// Background filter processor.
+    private let backgroundFilterProcessor: BackgroundFilterProcessor
+
+    /// Public constructor to initialize the processor with a `BackgroundReplacementConfiguration`.
+    ///
+    /// - Parameters:
+    ///   - backgroundReplacementConfiguration: `BackgroundReplacementConfiguration` class.
+    public init(backgroundReplacementConfiguration: BackgroundReplacementConfiguration) {
+        self.backgroundFilterProcessor = backgroundReplacementConfiguration.backgroundFilterProcessor
+
+        guard let backgroundReplacementImage = backgroundReplacementConfiguration.backgroundReplacementImage.cgImage
+        else {
+            backgroundFilterProcessor.logger.error(msg: "Faild to load CGImage of the provided background replacement image.")
+            return
+        }
+        self.backgroundReplacementImage = CIImage(cgImage: backgroundReplacementImage)
+    }
+
+    /// Receive a video frame from some upstream source. The foreground is segmented and then masked on top
+    /// of the background replacement image.
+    ///
+    /// - Parameters:
+    ///   - frame: New video frame to consume.
+    public func onVideoFrameReceived(frame: VideoFrame) {
+        var processedFrame: VideoFrame = frame
+
+        // Update sinks at the end.
+        defer {
+            updateSinks(frame: processedFrame)
+        }
+
+        // Create `VideoFramePixelBuffer` of the input frame.
+        guard let pixelBuffer = frame.buffer as? VideoFramePixelBuffer else {
+            backgroundFilterProcessor.logger.debug(debugFunction: {
+                return "VideoFramePixelBuffer was not found."
+            })
+            return
+        }
+
+        // CIImage of the input pixel buffer.
+        let inputFrame = CIImage(cvImageBuffer: pixelBuffer.pixelBuffer)
+
+        guard let inputCgFrame = context.createCGImage(inputFrame, from: inputFrame.extent) else {
+            backgroundFilterProcessor.logger.error(msg: "Error creating CGImage of input frame.")
+            return
+        }
+
+        // Retrieve the foreground alpha mask of the frame.
+        guard let foregroundMask = backgroundFilterProcessor.createForegroundAlphaMask(inputFrameCG: inputCgFrame,
+                                                                                       inputFrameCI: inputFrame)
+        else {
+            return
+        }
+
+        guard let backgroundImage = backgroundReplacementImage else {
+            backgroundFilterProcessor.logger.error(msg: "Error retrieving the background replacement image.")
+            return
+        }
+        // Create the final output image by blending the alpha mask on top of the input frame to produce
+        // the foreground image which is placed on top of the background replacement image.
+        guard let outputImage: CIImage = backgroundFilterProcessor.blendWithWithAlphaMask(inputFrameCI: inputFrame,
+                                                                                          maskImage: foregroundMask,
+                                                                                          backgroundImage: backgroundImage)
+        else {
+            backgroundFilterProcessor.logger.error(msg: "Error producing the final output image.")
+            return
+        }
+
+        // Create the final `CVPixelBuffer` for the output image.
+        var mergedImageBuffer: CVPixelBuffer?
+        guard let bufferPool = backgroundFilterProcessor.getBufferPool() else {
+            backgroundFilterProcessor.logger.error(msg: "Error retrieving final buffer pool.")
+            return
+        }
+        CVPixelBufferPoolCreatePixelBuffer(nil, bufferPool, &mergedImageBuffer)
+        guard let validMergedImageBuffer = mergedImageBuffer else {
+            backgroundFilterProcessor.logger.error(msg: "Error creating CVPixelBuffer for output image.")
+            return
+        }
+
+        // Render the output CGImage image in the buffer.
+        context.render(outputImage, to: validMergedImageBuffer)
+
+        processedFrame = VideoFrame(timestampNs: frame.timestampNs,
+                                    rotation: frame.rotation,
+                                    buffer: VideoFramePixelBuffer(pixelBuffer: validMergedImageBuffer))
+    }
+
+    /// Allow builders to change background image after initialization.
+    ///
+    /// - Parameters:
+    ///   - newBackgroundReplacementImage: New background replacement image.
+    public func setBackgroundImage(newBackgroundReplacementImage: UIImage) {
+        guard let backgroundImage = newBackgroundReplacementImage.cgImage else {
+            backgroundFilterProcessor.logger.error(msg: "Error trying to change background replacement image.")
+            return
+        }
+        backgroundReplacementImage = CIImage(cgImage: backgroundImage)
+    }
+
+    /// Adds a video sink to the sinks set.
+    public func addVideoSink(sink: VideoSink) {
+        if !sinks.contains(sink) {
+            sinks.add(sink)
+        }
+    }
+
+    /// Remove a video sink from the sinks set.
+    public func removeVideoSink(sink: VideoSink) {
+        if sinks.contains(sink) {
+            sinks.remove(sink)
+        }
+    }
+
+    /// Update the `VideoSink(s)` with a new frame.
+    ///
+    /// - Parameters:
+    ///    - frame: Next frame to render.
+    public func updateSinks(frame: VideoFrame) {
+        sinks.forEach({ sink in
+            (sink as? VideoSink)?.onVideoFrameReceived(frame: frame)
+        })
+    }
+}
