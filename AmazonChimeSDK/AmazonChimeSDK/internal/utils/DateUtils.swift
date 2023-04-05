ec4e23f62785af127f2e516e177f05527cbe4d56
AMAN GUPTA <123211697+aman-aguptaaa@users.noreply.github.com>
2023-04-05 15:51:07 -0700

    Pass client UTC offset to audio and video client for metrics (#587)
    * Pass client UTC offset to audio and video client for metrics
    
    * Address revision 1 comments
    
    * Fix stash conflicts
    
    * Remove TimezoneUtils

--- AmazonChimeSDK/AmazonChimeSDK/internal/utils/DateUtils.swift
+++ AmazonChimeSDK/AmazonChimeSDK/internal/utils/DateUtils.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 class DateUtils {
+    private static let utcOffsetFormat = "%@%.2d:%.2d"
+
+    static func getFormattedUtcOffset(offsetSeconds: Int) -> String {
+        let offsetHours = abs(offsetSeconds / 60) / 60
+        let offsetMinutes = abs(offsetSeconds / 60) % 60
+        let offsetSign = offsetSeconds < 0 ? "-" : "+"
+        let offset = String(format: utcOffsetFormat, offsetSign, offsetHours, offsetMinutes)
+        return offset
+    }
+    
     static func getCurrentTimeStampMs() -> Int64 {
         return Int64(Date().timeIntervalSince1970 * 1000)
     }
