c6b7c0cc000de97e3fcc9109330f824837b5cf22
George Zhang <georgezy@amazon.com>
2024-05-02 17:02:03 -0700

    MeetingSessionStatusCode comments updated

--- AmazonChimeSDK/AmazonChimeSDK/session/MeetingSessionStatusCode.swift
+++ AmazonChimeSDK/AmazonChimeSDK/session/MeetingSessionStatusCode.swift
@@ -12,46 +12,48 @@ import Foundation
     /// Everything is OK so far.
     case ok = 0
 
-    /// The audio leg failed.
+    /// There was an internal server error related to audio. This may indicate some issue with the audio device, or an issue with the Amazon Chime SDK service itself.
     case audioDisconnected = 9
 
     /// Due to connection health a reconnect has been triggered.
     case connectionHealthReconnect = 10
 
-    /// Network is not good enough for VoIP.
+    /// Network is not good enough for VoIP, `AudioVideoObserver.audioSessionDidDrop()` will be triggered, and there will an automatic attempt of reconnecting.
+    /// If the reconnecting is successful, `onAudioSessionStarted` will be called with value of reconnecting as true.
     case networkBecomePoor = 59
 
-    /// Server hung up.
+    /// Chime SDK audio server hung up.
     case audioServerHungup = 60
 
     /// The attendee joined from another device.
     case audioJoinedFromAnotherDevice = 61
 
-    /// There was an internal server error with the audio leg.
+    /// There was an internal server error related to audio. This may indicate some issue with the audio device, or an issue with the Amazon Chime SDK service itself
     case audioInternalServerError = 62
 
-    /// Authentication was rejected. The client is not allowed on this call.
+    /// Authentication was rejected as the attendee information in MeetingSessionCredentials did not match that of an attendee created via chime::CreateAttendee.
+    /// This error may imply an issue with your credential providing service, the client will not allowed on this call.
     case audioAuthenticationRejected = 63
 
-    /// The client can not join because the call is at capacity.
+    /// The client can not join because the meeting is at capacity. The service supports up to 250 attendees.
     case audioCallAtCapacity = 64
 
-    /// Could not connect the audio leg due to the service being unavailable.
+    /// There was an internal server error related to audio. This may indicate some issue with the audio device, or an issue with the Amazon Chime SDK service itself.
     case audioServiceUnavailable = 65
 
     /// The attendee should explicitly switch itself from joined with audio to checked-in.
     case audioDisconnectAudio = 69
 
-    /// The call was ended.
+    /// The attendee attempted to join a meeting that has already ended. See this [FAQ](https://aws.github.io/amazon-chime-sdk-js/modules/faqs.html#when-does-an-amazon-chime-sdk-meeting-end) for more information. The end user may want to be notified of this type of error.
     case audioCallEnded = 75
 
-    /// video service is unavailable.
+    /// There was an internal server error related to video. This may indicate some issue with the video device, or an issue with the Amazon Chime SDK service itself.
     case videoServiceUnavailable = 12
 
-    /// If State cannot be parsed, then use this state.
+    /// The meeting session is in unkown status.
     case unknown = 78
 
-    /// When maximum concurrent video channel reached
+    /// The video client has tried to send video but was unable to do so due to capacity reached. However, the video client can still receive remote video streams.
     case videoAtCapacityViewOnly = 206
 
     /// Designated input device is not responding and timed out.
