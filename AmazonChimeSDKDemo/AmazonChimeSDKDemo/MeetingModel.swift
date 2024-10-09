c6a7671f6f9505427c3b958b956f50a455b5bc01
Ziyi Zhang <ziyiz@amazon.com>
2024-10-09 13:28:26 -0700

    Revert "End demo meeting faster when video service unavailable (#693)"
    This reverts commit f04f559e82f561be6477d47bddecfb992d8f3a43.

--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/MeetingModel.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/MeetingModel.swift
@@ -402,9 +402,6 @@ extension MeetingModel: AudioVideoObserver {
 
     func videoSessionDidStopWithStatus(sessionStatus: MeetingSessionStatus) {
         logWithFunctionName(message: "\(sessionStatus.statusCode)")
-        if sessionStatus.statusCode == .videoServiceUnavailable {
-            audioSessionDidStopWithStatus(sessionStatus: sessionStatus)
-        }
     }
 
     func audioSessionDidStartConnecting(reconnecting: Bool) {
