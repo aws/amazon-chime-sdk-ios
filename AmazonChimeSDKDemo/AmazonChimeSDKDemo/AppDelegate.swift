8143255018e9a84005bd2051a5e40401929b4930
George Zhang <georgezy@amazon.com>
2025-01-13 19:26:13 -0800

    Add Firebase crashlytics

--- AmazonChimeSDKDemo/AmazonChimeSDKDemo/AppDelegate.swift
+++ AmazonChimeSDKDemo/AmazonChimeSDKDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
             self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
         }
+        FirebaseApp.configure()
         return true
     }
 
